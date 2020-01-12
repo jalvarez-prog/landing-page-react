838a13177fe4e5fa595d93cfb3089ab4db514925
Juan Alvarez <jcalvarezmeneses@gmail.com>
2020-01-12 13:20:08 -0300

    Home y Nabvar

--- src/views/home.js
+++ src/views/home.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import Navbar from './../components/navbar';
 
 const Home = (props) => {
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12 d-flex justify-content-center">
-                    <i className="fab fa-html5 fa-5x"></i>
+        <>
+            <Navbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 d-flex justify-content-center">
+                        <i className="fab fa-html5 fa-5x"></i>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12 d-flex justify-content-center">
+                        <p className="text-success">Welcome to Webpack, Bootstrap and ReactJS</p>
+                    </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-md-12 d-flex justify-content-center">
-                    <p className="text-success">Welcome to Webpack, Bootstrap and ReactJS</p>
-                </div>
-            </div>
-        </div>
+        </>
     )
 }
 
